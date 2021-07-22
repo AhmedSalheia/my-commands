b8f1ab73e2cb21018343b6575fd4a7c5e271787b
AhmedSalheia <ahmedsalheia.as@gmail.com>
2021-07-22 23:55:53 +0300

    updates

--- laravel/artisan.bat
+++ laravel/artisan.bat
@@ -1,11 +1,4 @@
 @echo off
 
-IF [%*]==[] goto halt
-
 CALL php artisan %*
-goto :EOF
-
-
-:halt
-echo Params Provided
 goto :EOF
---
+++ vue/vue.create.bat
@@ -0,0 +1,62 @@
+@echo off
+
+IF [%1]==[] goto haltErrNoProjectName
+set projectName=%1
+
+@FOR /F "tokens=*" %%i IN ('convertStringCase %projectName% lower') DO set projectName=%%i
+
+if exist %projectName% (
+    echo project already exists
+    goto :EOF
+)
+
+mkdir %projectName%
+cd %projectName%
+
+CALL type %~dp0files\.jsconfig > jsconfig.json 
+
+CALL npm ls -g @vue/cli | find "@vue/cli">nul
+if [%errorLevel%]==[1] (
+    echo isntalling vue-cli globally
+    CALL npm i -g @vue/cli
+    set errorLevel=0
+)
+
+CALL vue create .
+CALL npm i normalize.css bootstrap jquery @popperjs/core swiper laravel-echo pusher-js axion @vueuse/head @fortawesome/fontawesome-svg-core @fortawesome/free-solid-svg-icons @fortawesome/vue-fontawesome nprogress lodash --save
+
+if not exist "src" (
+    mkdir src
+)
+if not exist "src\init" (
+    mkdir src\init
+)
+if not exist "src\global\components" (
+    mkdir src\global\components
+)
+if not exist "src\components\layouts" (
+    mkdir src\components\layouts
+)
+CALL type %~dp0files\.main > src\main.js
+CALL type %~dp0files\.init > src\init\index.js
+CALL type %~dp0files\.global > src\global\components\globalComponents.js
+
+CALL npm ls -g prettier | find /I /C "prettier" >nul
+if [%errorLevel%]==[1] (
+    echo isntalling prettier globally
+    CALL npm i -g prettier
+    set errorLevel=0
+)
+
+CALL echo node_modules/ > .prettierignore
+CALL prettier --write .
+
+echo Referrences:
+echo    swiper:  https://swiperjs.com/vue
+echo    vue-meta: https://vue-meta.nuxtjs.org/
+echo    fontawesome: https://vuecomponent.com/integrations/fontawesome.html
+goto :EOF
+
+:haltErrNoProjectName
+echo No Project Name Provided
+goto :EOF
